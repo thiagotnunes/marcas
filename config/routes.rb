c5e89b1268d43195f0f9114e93114ee8e74511ec
Thiago Nunes <thiagotnunes@gmail.com>
2012-03-09 14:06:17 -0300

    Adding home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 Marcas::Application.routes.draw do
-  get "password_resets/create"
-
-  get "password_resets/edit"
-
-  get "password_resets/update"
-
   get "logout" => "sessions#destroy", :as => "logout"
   get "login" => "sessions#new", :as => "login"
   post "login" => "sessions#create", :as => "login"
   get "signup" => "users#new", :as => "signup"
 
-  root :to => "users#index"
+  get "index" => "home#index", :as => "home"
+
+  root :to => "home#index"
 
   resources :users do
     member do
