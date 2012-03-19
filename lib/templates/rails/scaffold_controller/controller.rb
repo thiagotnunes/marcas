4a601b99e10071825a5c57f6695ef47263f29379
Thiago Nunes <thiagotnunes@gmail.com>
2012-03-19 17:18:30 -0300

    Adding generator for controllers

---
+++ lib/templates/rails/scaffold_controller/controller.rb
@@ -0,0 +1,44 @@
+class <%= controller_class_name %>Controller < ApplicationController
+<% unless options[:singleton] -%>
+  def index
+    @<%= table_name %> = <%= orm_class.all(class_name) %>
+  end
+<% end -%>
+
+  def show
+    @<%= file_name %> = <%= orm_class.find(class_name, "params[:id]") %>
+  end
+
+  def new
+    @<%= file_name %> = <%= orm_class.build(class_name) %>
+  end
+
+  def edit
+    @<%= file_name %> = <%= orm_class.find(class_name, "params[:id]") %>
+  end
+
+  def create
+    @<%= file_name %> = <%= orm_class.build(class_name, "params[:#{file_name}]") %>
+
+    if @<%= orm_instance.save %>
+      redirect_to @<%= singular_table_name %>, notice: t('<%= singular_table_name %>.messages.created')
+    else
+      render :new
+    end
+  end
+
+  def update
+    @<%= file_name %> = <%= orm_class.find(class_name, "params[:id]") %>
+
+    if @<%= orm_instance.update_attributes("params[:#{singular_table_name}]") %>
+      redirect_to @<%= singular_table_name %>, notice: t('<%= singular_table_name %>.messages.updated')
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @<%= file_name %> = <%= orm_class.find(class_name, "params[:id]") %>
+    @<%= orm_instance.destroy %>
+  end
+end
