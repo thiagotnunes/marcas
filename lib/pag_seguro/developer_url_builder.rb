e8f376504e20ec47e21484914b7309d117fd7fb3
Thiago Nunes <thiagotnunes@gmail.com>
2012-04-16 13:42:10 -0300

    Adding developer url builder

---
+++ lib/pag_seguro/developer_url_builder.rb
@@ -0,0 +1,17 @@
+module PagSeguro
+  class DeveloperUrlBuilder
+    FILE_PATH = File.join(Rails.root, "tmp", "pagseguro-#{Rails.env}.yml")
+
+    def url_for(order)
+      FileUtils.touch(FILE_PATH) unless File.exist?(FILE_PATH)
+
+      orders = YAML.load_file(FILE_PATH) || {}
+
+      orders[order.invoice.id] = order.pagseguro_billing_data.except("reference")
+
+      File.open(FILE_PATH, "w+") do |f|
+        f << orders.to_yaml
+      end
+    end
+  end
+end
---
+++ spec/lib/pag_seguro/developer_url_builder_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+describe PagSeguro::DeveloperUrlBuilder do
+  let(:file_path) { PagSeguro::DeveloperUrlBuilder::FILE_PATH }
+  subject { PagSeguro::DeveloperUrlBuilder.new }
+
+  before :each do
+    File.unlink(file_path) if File.exist?(file_path)
+  end
+
+  it "should create the file if it does not exist" do
+    subject.url_for(FactoryGirl.create(:order))
+
+    File.should be_file(file_path)
+  end
+
+  it "should not create the file again if it already exists" do
+    FileUtils.touch(file_path)
+    FileUtils.should_not_receive(:touch)
+
+    subject.url_for(FactoryGirl.create(:order))
+
+    File.should be_file(file_path)
+  end
+
+  it "should record order information in file" do
+    order = FactoryGirl.create(:order)
+    config = {
+      "email" => "email",
+      "authenticity_token" => "token"
+    }
+    PagSeguro.stub(:config).and_return(config)
+
+    subject.url_for(order)
+
+    orders = YAML.load_file(file_path)
+    actual = orders[order.invoice.id]
+
+    actual["email"].should == "email"
+    actual["token"].should == "token"
+    actual["currency"].should == "BRL"
+    actual["itemQuantity1"].should == "1"
+    actual["itemId1"].should == order.id
+    actual["itemDescription1"].should == order.service.name
+    actual["itemAmount1"].should == "%.2f" % order.service.price
+  end
+end
