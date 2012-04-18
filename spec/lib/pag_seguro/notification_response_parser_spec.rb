96e22f89a33ca1d3ad7e8a604600f1a2f7a46009
Thiago Nunes <thiagotnunes@gmail.com>
2012-04-18 15:48:25 -0300

    Refactoring tests for response parser notification

--- spec/lib/pag_seguro/notification_response_parser_spec.rb
+++ spec/lib/pag_seguro/notification_response_parser_spec.rb
@@ -5,67 +5,31 @@ describe PagSeguro::NotificationResponseParser do
   let(:file_path) { File.join('spec', 'lib', 'pag_seguro', 'responses') }
 
   it "should get status aguardando pagamento for 1" do
-    xml = File.open(File.join(file_path, 'aguardando_pagamento.xml'))
-    response = stub(:code => "200", :body => xml.read)
-    notification_response = PagSeguro::NotificationResponseParser.new(response)
-
-    notification_response.status.should == :aguardando_pagamento
+    assert_notification("REF1234", :aguardando_pagamento)
   end
 
   it "should get status em analise for 2" do
-    xml = File.open(File.join(file_path, 'em_analise.xml'))
-    response = stub(:code => "200", :body => xml.read)
-    notification_response = PagSeguro::NotificationResponseParser.new(response)
-
-    notification_response.status.should == :em_analise
+    assert_notification("REF1234", :em_analise)
   end
 
   it "should get status paga for 3" do
-    xml = File.open(File.join(file_path, 'paga.xml'))
-    response = stub(:code => "200", :body => xml.read)
-    notification_response = PagSeguro::NotificationResponseParser.new(response)
-
-    notification_response.status.should == :paga
+    assert_notification("REF1234", :paga)
   end
 
   it "should get status disponivel for 4" do
-    xml = File.open(File.join(file_path, 'disponivel.xml'))
-    response = stub(:code => "200", :body => xml.read)
-    notification_response = PagSeguro::NotificationResponseParser.new(response)
-
-    notification_response.status.should == :disponivel
+    assert_notification("REF1234", :disponivel)
   end
 
   it "should get status em disputa for 5" do
-    xml = File.open(File.join(file_path, 'em_disputa.xml'))
-    response = stub(:code => "200", :body => xml.read)
-    notification_response = PagSeguro::NotificationResponseParser.new(response)
-
-    notification_response.status.should == :em_disputa
+    assert_notification("REF1234", :em_disputa)
   end
 
   it "should get status devolvida for 6" do
-    xml = File.open(File.join(file_path, 'devolvida.xml'))
-    response = stub(:code => "200", :body => xml.read)
-    notification_response = PagSeguro::NotificationResponseParser.new(response)
-
-    notification_response.status.should == :devolvida
+    assert_notification("REF1234", :devolvida)
   end
 
   it "should get status cancelada for 7" do
-    xml = File.open(File.join(file_path, 'cancelada.xml'))
-    response = stub(:code => "200", :body => xml.read)
-    notification_response = PagSeguro::NotificationResponseParser.new(response)
-
-    notification_response.status.should == :cancelada
-  end
-
-  it "should get order id based on the transaction reference" do
-    xml = File.open(File.join(file_path, 'paga.xml'))
-    response = stub(:code => "200", :body => xml.read)
-    notification_response = PagSeguro::NotificationResponseParser.new(response)
-
-    notification_response.reference.should == "REF1234"
+    assert_notification("REF1234", :cancelada)
   end
 
   it "should raise an error when response is not ok" do
@@ -74,5 +38,14 @@ describe PagSeguro::NotificationResponseParser do
     lambda {
       PagSeguro::NotificationResponseParser.new(response)
     }.should raise_error
+  end
+
+  def assert_notification(reference, status)
+    xml = File.open(File.join(file_path, "#{status}.xml"))
+    response = stub(:code => "200", :body => xml.read)
+    notification_response = PagSeguro::NotificationResponseParser.new(response)
+
+    notification_response.reference.should == reference
+    notification_response.status.should == status
   end
 end
