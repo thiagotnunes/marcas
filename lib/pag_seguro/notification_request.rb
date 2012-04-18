46208ebc7b88f7624e88f98ba8dce080e990d096
Thiago Nunes <thiagotnunes@gmail.com>
2012-04-18 11:20:16 -0300

    Notification request will be responsible for retrieving the content of a notification

--- lib/pag_seguro/notification_request.rb
+++ lib/pag_seguro/notification_request.rb
@@ -1,10 +1,30 @@
+require 'uri'
+require 'net/https'
+
 module PagSeguro
   class NotificationRequest
+    URL = "https://ws.pagseguro.uol.com.br/v2/transactions/notifications"
+
     attr_reader :code, :type
 
     def initialize(attributes)
       @code = attributes["notificationCode"]
       @type = attributes["notificationType"]
+    end
+
+    def check_data
+      uri = URI.parse(request_url)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      request = Net::HTTP::Get.new(uri.request_uri)
+
+      http.request(request)
+    end
+
+    private
+
+    def request_url
+      "#{URL}/#{@code}?email=#{PagSeguro.config["email"]}&token=#{PagSeguro.config["token"]}"
     end
   end
 end
--- spec/lib/pag_seguro/notification_request_spec.rb
+++ spec/lib/pag_seguro/notification_request_spec.rb
@@ -1,15 +1,39 @@
-require_relative '../../../lib/pag_seguro/notification'
+require 'uri'
+require 'net/https'
+require_relative '../../../lib/pag_seguro/notification_request'
 
 describe PagSeguro::NotificationRequest do
-  it "should create a notification request from hash" do
+
+  let(:notification) do 
     attributes = {
       "notificationCode" => "code",
       "notificationType" => "type"
     }
+    PagSeguro::NotificationRequest.new(attributes)
+  end
 
-    notification = PagSeguro::NotificationRequest.new(attributes)
-
+  it "should create a notification request from hash" do
     notification.code.should == "code"
     notification.type.should == "type"
+  end
+
+  it "should make a request to pagseguro" do
+    uri = stub(:host => "host", :port => 3000, :request_uri => "uri")
+    req = stub
+    resp = stub
+    http = stub
+    config = {
+      "email" => "email",
+      "token" => "token"
+    }
+    PagSeguro.stub(:config).and_return(config)
+
+    URI.stub(:parse).with("https://ws.pagseguro.uol.com.br/v2/transactions/notifications/code?email=email&token=token").and_return(uri)
+    Net::HTTP.stub(:new).with(uri.host, uri.port).and_return(http)
+    Net::HTTP::Get.stub(:new).with(uri.request_uri).and_return(req)
+    http.should_receive(:use_ssl=).with(true)
+    http.should_receive(:request).with(req).and_return(resp)
+
+    notification.check_data.should == resp
   end
 end
